b830f3e6b593f4ac097189660182ebc6af89f364
Dayanandayya <01fe22bca036@kletech.ac.in>
2024-02-03 12:06:07 +0530

    Update the code for GeoArea

--- cal.java
+++ cal.java
@@ -1,34 +1,44 @@
-class cal{
+class cal extends GeoArea{
             
     public int add(int a,int b)
     {
-        return =a+b;
+        return a+b;
     }
-    public  int sub(float a,int b)
+    public  int sub(int a,int b)
     {
-        return =a-b;
+        return a-b;
     }
-    public int prod(float a,int b)
+    public int prod(int a,int b)
     {
-        return =a*b;
+        return a*b;
     }
     public int div(int a,int b)
     {
-        return =a/b;
+        return a/b;
     }
 
     
 
-
+    
 }
 class mainclass{
     public static void main(String args[]){
-        cal a=cal();
+        float operand_1=10;
+        float operand_2=30;
+        cal a= new cal();
+       
         
         System.out.println("Add ="+a.add(20,10));
         System.out.println(" sub is="+a.sub(20,5));
         System.out.println("multi is  ="+a.prod(6,10));
         System.out.println("div is ="+a.div(20,10));
-
+        
+        double area_triangle =a.area_of_triangle(operand_1,operand_2);
+        System.out.println("Area of Triangle is :" +area_triangle);
+        
+        double area_rectangle =a.area_of_rectangle(operand_1,operand_2);
+        System.out.println("Area of Rectangle is :" +area_rectangle);
     }
-}
+}    
+
+
