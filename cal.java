367b7e4066eb22d53788a90ac02668c39fac0abc
Dayanandayya <01fe22bca036@kletech.ac.in>
2024-02-03 11:18:34 +0530

    code for calculator

--- cal.java
+++ cal.java
@@ -2,33 +2,56 @@ class cal{
             
     public int add(int a,int b)
     {
-        return =a+b;
+        return a+b;
     }
-    public  int sub(float a,int b)
+    public  int sub(int a,int b)
     {
-        return =a-b;
+        return a-b;
     }
-    public int prod(float a,int b)
+    public int prod(int a,int b)
     {
-        return =a*b;
+        return a*b;
     }
     public int div(int a,int b)
     {
-        return =a/b;
+        return a/b;
     }
 
     
 
-
+    
 }
-class mainclass{
+class mainclass extends cal{
     public static void main(String args[]){
-        cal a=cal();
+        cal a= new cal();
+        areaof_triangle b= new areaof_triangle();
+        areasquare c= new areasquare();
+        b.area_triangle();
+        c.area_squre();
         
         System.out.println("Add ="+a.add(20,10));
         System.out.println(" sub is="+a.sub(20,5));
         System.out.println("multi is  ="+a.prod(6,10));
         System.out.println("div is ="+a.div(20,10));
+        
 
     }
+}
+
+class areaof_triangle extends cal{
+    void area_triangle(){
+        int a=5, b=10;
+        System.out.println("Area of Rectangle is " +0.5*a*b);
+
+    }
+
+    
+}
+class areasquare extends cal{
+    void area_squre(){
+        int r=10;
+        System.out.println("Area of Squre is " +r*r);
+
+    }
+
 }
