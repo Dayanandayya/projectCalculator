05b8b17d360a2f831d626965ec41016fc4048d76
Dayanandayya <01fe22bca036@kletech.ac.in>
2024-02-09 00:04:40 +0530

    Math Oprations

---
+++ Math_oprs.java
@@ -0,0 +1,20 @@
+public class Math_oprs {
+    double squre(float opr1){
+        return Math.sqrt(opr1);
+    }
+    double Max(float opr1,float opr2){
+        return Math.max(opr1,opr2);
+    }
+    double Min(float opr1,float opr2){
+        return Math.min(opr1,opr2);
+    }
+    double power(float opr1,float opr2){
+        return Math.pow(opr1,opr2);
+    }
+    double Round(float opr1){
+        return Math.round(opr1);
+    }
+    double Absolute(float opr1){
+        return Math.abs(opr1);
+    }
+}
