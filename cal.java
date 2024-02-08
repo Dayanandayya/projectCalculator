b1f5b052cc3602141150e229815b9b6879693784
Dayanandayya <01fe22bca036@kletech.ac.in>
2024-02-09 00:03:35 +0530

    change value for Math operations

--- cal.java
+++ cal.java
@@ -1,4 +1,4 @@
-class cal{
+class cal extends Math_oprs{
             
     public int add(int a,int b)
     {
@@ -23,6 +23,8 @@ class cal{
 }
 class mainclass{
     public static void main(String args[]){
+        float opr1=10;
+        float opr2=20;
         cal a= new cal();
        
         
@@ -30,8 +32,21 @@ class mainclass{
         System.out.println(" sub is="+a.sub(20,5));
         System.out.println("multi is  ="+a.prod(6,10));
         System.out.println("div is ="+a.div(20,10));
-        
 
+        double squre =a.squre(opr1);
+        System.out.println("Squre of Number  is :" +squre);
+        
+        double Min =a.Min(opr1,opr2);
+        System.out.println("Minimum Number is :"+Min);
+        double Max =a.Max(opr1,opr2);
+        System.out.println("Maximum  Number is :"+Max);
+        double power =a.power(opr1,opr2);
+        System.out.println("Power of Number is :"+power);
+        double Absolute =a.Absolute(opr1);
+        System.out.println(" Absolute value is " +Absolute);
+        double Round=a.Round(opr1);
+        System.out.println("Round  to the nearest integer is:" +Round);
+    
     }
 }
 
