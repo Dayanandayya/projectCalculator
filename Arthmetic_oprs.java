bbe0d79d28c4600454760f6470ca8c6584003489
Dayanandayya <01fe22bca036@kletech.ac.in>
2024-02-08 22:31:56 +0530

    Calculating Arthmetic opration

---
+++ Arthmetic_oprs.java
@@ -0,0 +1,17 @@
+
+public class Arthmetic_oprs {
+    double squre(float opr1 ){
+        return opr1*opr1;
+    }
+    double cube(float opr1){
+        return opr1*opr1*opr1;
+    }
+    double module(float opr1,float opr2){
+        return opr1 % opr2;
+    }
+    double power(float opr1,float opr2){
+        return Math.pow(opr1, opr2);
+    }
+
+    
+}
